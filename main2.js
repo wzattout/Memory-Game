7dc2acad24668c215361b17c5e093934ca167204
ziadabdelkader <43037813+ziadabdelkader@users.noreply.github.com>
2019-06-24 04:25:50 +0200

    make buttons resposive

--- main2.js
+++ main2.js
@@ -1,61 +1,98 @@
 /*all is array for all images in document*/
 var all=document.querySelectorAll('.backCard');
+var restart=document.getElementById("restart");
+var solve=document.getElementById("solve");
+var imageArray2;
+/*flag to keep track of is it the first or second image to click*/
+var flag=0;
+/*save clicked images */
+var image1,image2;
 
-/* implementation of 2 arrays with my size*/
-var imageArray=new Array();
-for(var i=0;i<36;i++) imageArray[i]=new Image();
-var imageArray2=new Array();
-for(var i=0;i<36;i++) imageArray2[i]=new Image();
+preStart();
 
-/* I will put all my images in one array (imageArray)*/
-                /*we sould add 36 images here*/
+
+function preStart(){
+    restart.innerHTML="Start";
+    restart.removeEventListener('click',preStart);
+    restart.addEventListener('click',start);
+    for(var i=0;i<36;i++)    
+        all[i].addEventListener('click',start);
+    /* implementation of 2 arrays with my size*/
+        var imageArray=new Array();
+        for(var i=0;i<36;i++) imageArray[i]=new Image();
+        imageArray2=new Array();
+        for(var i=0;i<36;i++) imageArray2[i]=new Image();
+        
+        /* I will put all my images in one array (imageArray)*/
+            imageArray[0].src='images/image1.jpg';
+            imageArray[1].src='images/image2.jpg';
+            imageArray[2].src='images/image3.jpg';
+            imageArray[3].src='images/image4.jpg';
+            imageArray[4].src='images/image5.jpg';
+            imageArray[5].src='images/image6.jpg';
+            imageArray[6].src='images/image7.jpg';
+            imageArray[7].src='images/image8.jpg';
+            imageArray[8].src='images/image9.jpg';
+            imageArray[9].src='images/image10.jpg';
+            imageArray[10].src='images/image11.jpg';
+            imageArray[11].src='images/image12.jpg';
+            imageArray[12].src='images/image13.jpg';
+            imageArray[13].src='images/image14.jpg';
+            imageArray[14].src='images/image15.jpg';
+            imageArray[15].src='images/image16.jpg';
+            imageArray[16].src='images/image17.jpg';
+            imageArray[17].src='images/image18.jpg';
+            imageArray[18].src='images/image1.jpg';
+            imageArray[19].src='images/image2.jpg';
+            imageArray[20].src='images/image3.jpg';
+            imageArray[21].src='images/image4.jpg';
+            imageArray[22].src='images/image5.jpg';
+            imageArray[23].src='images/image6.jpg';
+            imageArray[24].src='images/image7.jpg';
+            imageArray[25].src='images/image8.jpg';
+            imageArray[26].src='images/image9.jpg';
+            imageArray[27].src='images/image10.jpg';
+            imageArray[28].src='images/image11.jpg';
+            imageArray[29].src='images/image12.jpg';
+            imageArray[30].src='images/image13.jpg';
+            imageArray[31].src='images/image14.jpg';
+            imageArray[32].src='images/image15.jpg';
+            imageArray[33].src='images/image16.jpg';
+            imageArray[34].src='images/image17.jpg';
+            imageArray[35].src='images/image18.jpg';
+            for(var i=0;i<36;i++){
+                /* moving images to another array(imageArray2) in random order*/
+                var rand=Math.floor(Math.random() * (36-i));
+                imageArray2[i]=imageArray[rand];
+                imageArray.splice(rand,1);
+                //flip all cards to back
+                all[i].src='images/cardback.jpg';
+            }
+            solve.removeEventListener('click',solveClick);
+}
+function start(e){
+    solve.addEventListener('click',solveClick);
+    restart.removeEventListener('click',start);
+    restart.addEventListener('click',preStart);
+    for(var i=0;i<36;i++)    
+        all[i].removeEventListener('click',start);
     
-    imageArray[0].src='images/image1.jpg';
-    imageArray[1].src='images/image2.jpg';
-    imageArray[2].src='images/image3.jpg';
-    imageArray[3].src='images/image4.jpg';
-    imageArray[4].src='images/image5.jpg';
-    imageArray[5].src='images/image6.jpg';
-    imageArray[6].src='images/image7.jpg';
-    imageArray[7].src='images/image8.jpg';
-    imageArray[8].src='images/image9.jpg';
-    imageArray[9].src='images/image10.jpg';
-    imageArray[10].src='images/image11.jpg';
-    imageArray[11].src='images/image12.jpg';
-    imageArray[12].src='images/image13.jpg';
-    imageArray[13].src='images/image14.jpg';
-    imageArray[14].src='images/image15.jpg';
-    imageArray[15].src='images/image16.jpg';
-    imageArray[16].src='images/image17.jpg';
-    imageArray[17].src='images/image18.jpg';
-    imageArray[18].src='images/image1.jpg';
-    imageArray[19].src='images/image2.jpg';
-    imageArray[20].src='images/image3.jpg';
-    imageArray[21].src='images/image4.jpg';
-    imageArray[22].src='images/image5.jpg';
-    imageArray[23].src='images/image6.jpg';
-    imageArray[24].src='images/image7.jpg';
-    imageArray[25].src='images/image8.jpg';
-    imageArray[26].src='images/image9.jpg';
-    imageArray[27].src='images/image10.jpg';
-    imageArray[28].src='images/image11.jpg';
-    imageArray[29].src='images/image12.jpg';
-    imageArray[30].src='images/image13.jpg';
-    imageArray[31].src='images/image14.jpg';
-    imageArray[32].src='images/image15.jpg';
-    imageArray[33].src='images/image16.jpg';
-    imageArray[34].src='images/image17.jpg';
-    imageArray[35].src='images/image18.jpg';
-
-                /*we sould add 36 images here*/
+        restart.innerHTML="restart";
+    for(var i=0;i<36;i++){
+        //remove event listners and readd them 
+        all[i].removeEventListener('click',myFunction);
+        all[i].addEventListener('click',myFunction);
+    }
+    flag=0;
     
-
-
-/* moving images to another array(imageArray2) in random order*/
-for(var i=0;i<36;i++){
-    var rand=Math.floor(Math.random() * (36-i));
-    imageArray2[i]=imageArray[rand];
-    imageArray.splice(rand,1);
+    if(event.target.innerHTML!="restart"){
+        var i=0;
+        while(event.target!=all[i]) {i++;}
+        event.target.src=imageArray2[i].src;
+        image1=event.target;
+        event.target.removeEventListener('click',myFunction);
+        flag=1;
+    }
 }
 /* function wait make system wait for ms mile seconds*/
 function wait(ms){
@@ -65,11 +102,7 @@ function wait(ms){
       end = new Date().getTime();
    }
  }
-/* core of implementation */
-/*flag to keep track of is it the first or second image to click*/
-var flag=0;
-/*prev to save last image clicked */
-var image1,image2;
+
 /* myFunction1 just flip the clicked image*/
 function myFunction(e) {
     var i=0;
@@ -91,17 +124,14 @@ function myFunction(e) {
 }
 
 
-/* add event listener for all images in the document*/
-for(var j = 0; j < 36; j++) {
-    all[j].addEventListener('click',myFunction);
-}
+
 
 var intervalID = window.setInterval(myCallback, 500);
 
 function myCallback() {
     if(flag===2){
         if(image1.src!=image2.src){
-            wait(1000);
+            wait(500);
             image1.src='images/cardback.jpg';
             image2.src='images/cardback.jpg';
             image1.addEventListener('click',myFunction);
@@ -110,4 +140,10 @@ function myCallback() {
         flag=0;
     }
 }
-
+function solveClick(){
+    for(var i=0;i<36;i++){
+        all[i].src=imageArray2[i].src;
+        all[i].removeEventListener('click',myFunction);
+    }
+    solve.removeEventListener('click',solveClick);
+}
